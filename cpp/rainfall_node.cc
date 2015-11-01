f1b1468314fe91f2b142290e231e08571f3cbda3
smart__petea <smart__petea@mail.ru>
2015-11-01 09:34:11 +0200

    podlejit otladke

--- cpp/rainfall_node.cc
+++ cpp/rainfall_node.cc
@@ -1,6 +1,14 @@
 #include <nan.h>
 #include "rainfall.h"
 
+void pack_rain_result(v8::Local<v8::Object>& target, rain_result& result)
+{
+    Nan::Set(target, Nan::New("mean").ToLocalChecked(), Nan::New<v8::Number>(result.mean));
+    Nan::Set(target, Nan::New("standard_deviation").ToLocalChecked(), Nan::New(result.standard_deviation));
+    Nan::Set(target, Nan::New("median").ToLocalChecked(), Nan::New<v8::Number>(result.median));
+    Nan::Set(target, Nan::New("n").ToLocalChecked(), Nan::New<v8::Integer>(result.n));
+}
+
 sample unpack_sample(v8::Handle<v8::Object> sample_obj) {
     sample s;
     v8::Handle<v8::Value> date_Value = Nan::Get(sample_obj, Nan::New("date").ToLocalChecked()).ToLocalChecked();
@@ -13,10 +21,10 @@ sample unpack_sample(v8::Handle<v8::Object> sample_obj) {
     return s;
 }
 
-location unpack_location(Nan::NAN_METHOD_ARGS_TYPE info)
+
+location unpack_location(v8::Handle<v8::Object>& location_obj)
 {
     location loc;
-    v8::Handle<v8::Object> location_obj = v8::Handle<v8::Object>::Cast(info[0]);
     v8::Handle<v8::Value> lat_Value = Nan::Get(location_obj, Nan::New("latitutude").ToLocalChecked()).ToLocalChecked(); 
     v8::Handle<v8::Value> lon_Value = Nan::Get(location_obj, Nan::New("longitude").ToLocalChecked()).ToLocalChecked();
 
@@ -35,16 +43,19 @@ location unpack_location(Nan::NAN_METHOD_ARGS_TYPE info)
     return loc;
 }
 
+location unpack_location(Nan::NAN_METHOD_ARGS_TYPE info)
+{
+    v8::Handle<v8::Object> location_obj = v8::Handle<v8::Object>::Cast(info[0]);
+    return unpack_location(location_obj);
+}
+
 NAN_METHOD(RainfallData) {
     location loc = unpack_location(info);
     rain_result result = calc_rain_stats(loc);
 
     v8::Local<v8::Object> obj = Nan::New<v8::Object>();
 
-    Nan::Set(obj, Nan::New("mean").ToLocalChecked(), Nan::New<v8::Number>(result.mean));
-    Nan::Set(obj, Nan::New("standard_deviation").ToLocalChecked(), Nan::New(result.standard_deviation));
-    Nan::Set(obj, Nan::New("median").ToLocalChecked(), Nan::New<v8::Number>(result.median));
-    Nan::Set(obj, Nan::New("n").ToLocalChecked(), Nan::New<v8::Integer>(result.n));
+    pack_rain_result(obj, result);
 
     info.GetReturnValue().Set(obj);
 }
@@ -55,10 +66,47 @@ NAN_METHOD(AvgRainfall) {
     info.GetReturnValue().Set(Nan::New(avg));
 }
 
+NAN_METHOD(CalculateResults) {
+    std::vector<location> locations; //we'll get this from Node.js
+    std::vector<rain_result> results; //we'll build this in C++
+
+    v8::Local<v8::Array> input = v8::Local<v8::Array>::Cast(info[0]);
+
+    unsigned int num_locations = input->Length();
+    for(unsigned int i = 0; i < num_locations; i++)
+    {
+        v8::Local<v8::Object> m = v8::Local<v8::Object>::Cast(Nan::Get(input, Nan::New(i)).ToLocalChecked());
+        locations.push_back(
+                unpack_location(m)
+        );
+    }
+
+    results.resize(locations.size());
+    std::transform(
+            locations.begin(),
+            locations.end(),
+            results.begin(),
+            calc_rain_stats
+    );
+
+    //we'll populate this with the results
+    v8::Local<v8::Array> result_list = Nan::New<v8::Array>();
+
+    for(unsigned int i = 0; i < results.size(); i++)
+    {
+        v8::Local<v8::Object> result = Nan::New<v8::Object>();
+        pack_rain_result(result, results[i]);
+        Nan::Set(result_list, Nan::New(i), result);
+    }
+
+    info.GetReturnValue().Set(result_list);
+}
+
 
 NAN_MODULE_INIT(init) {
     Nan::Set(target, Nan::New("avg_rainfall").ToLocalChecked(), Nan::New<v8::FunctionTemplate>(AvgRainfall)->GetFunction());
     Nan::Set(target, Nan::New("data_rainfall").ToLocalChecked(), Nan::New<v8::FunctionTemplate>(RainfallData)->GetFunction());
+    Nan::Set(target, Nan::New("calculate_results").ToLocalChecked(), Nan::New<v8::FunctionTemplate>(CalculateResults)->GetFunction());
 }
 
 
--- rainfall.js
+++ rainfall.js
@@ -1,37 +1,36 @@
 var rainfall = require("./build/Release/rainfall.node");
-var location = {
-    latitude: 40.71,
-    longitude: -74.01,
-    samples: [
-        {
-            date: "2015-06-07",
-            rainfall: 2.1
-        },
-        {
-            date: "2015-06-14",
-            rainfall: 0.5
-        },
-        {
-            date: "2015-06-21",
-            rainfall: 1.5
-        },
-        {
-            date: "2015-06-28",
-            rainfall: 1.3
-        }, 
-        {
-            date: "2015-07-05",
-            rainfall: 0.9
-        }
-    ]
-};
 
-var avg = rainfall.avg_rainfall(location);
-console.log("Average rain fall = " + avg + "cm");
+var makeup = function(max) {
+    return Math.round(max * Math.random() * 100) / 100;
+}
 
-var data = rainfall.data_rainfall(location);
 
-console.log("Mean = " + data.mean);
-console.log("Median = " + data.median);
-console.log("Standard Deviation = " + data.standard_deviation);
-console.log("N = " + data.n);
+//Build some dummy locations
+var locations = [];
+for(var i = 0; i < 10; i++) {
+    var loc = {
+        latitude: makeup(180),
+        longitude: makeup(180),
+        samples: [
+            ]
+    }
+    locations.push(loc);
+}
+
+//Invoke the Addon
+var results = rainfall.calculate_results(locations);
+
+//report the results from C++
+var i = 0;
+results.forEach(function(result) {
+        console.log("Result for location " + i);
+        console.log("------------------------------");
+        console.log("\tLatitude:        " + locations[i].latitude.toFixed(2));
+        console.log("\tLongitude:       " + locations[i].longitude.toFixed(2));
+        console.log("\tMean Rainfall:   " + result.mean.toFixed(2) + " cm");
+        console.log("\tMedian Rainfall: " + result.median.toFixed(2) + " cm");
+        console.log("\tStandard dev.:   " + result.standard_deviation.toFixed(2));
+        console.log("\tNumber Samples:  " + result.n);
+        console.log();
+        i++;
+});

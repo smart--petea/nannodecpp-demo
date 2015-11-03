fe4b6d56824e653ad72f908ad52636a57b3ec166
smart__petea <smart__petea@mail.ru>
2015-11-03 21:21:27 +0200

    part 4

--- cpp/rainfall_node.cc
+++ cpp/rainfall_node.cc
@@ -1,5 +1,6 @@
 #include <nan.h>
 #include "rainfall.h"
+#include <unistd.h>
 
 void pack_rain_result(v8::Local<v8::Object>& target, rain_result& result)
 {
@@ -31,12 +32,12 @@ location unpack_location(v8::Handle<v8::Object>& location_obj)
     loc.latitude = lat_Value->NumberValue();
     loc.longitude = lon_Value->NumberValue();
 
-    v8::Handle<v8::Array> array = v8::Handle<v8::Array>::Cast(Nan::Get(location_obj, Nan::New("samples").ToLocalChecked()).ToLocalChecked());
+    v8::Handle<v8::Array> array = Nan::Get(location_obj, Nan::New("samples").ToLocalChecked()).ToLocalChecked().As<v8::Array>();
 
     int sample_count = array->Length();
 
     for(int i = 0; i < sample_count; i++) {
-        sample s = unpack_sample(v8::Handle<v8::Object>::Cast(Nan::Get(array, Nan::New(i)).ToLocalChecked()));
+        sample s = unpack_sample(Nan::Get(array, Nan::New(i)).ToLocalChecked().As<v8::Object>());
         loc.samples.push_back(s);
     }
 
@@ -45,7 +46,7 @@ location unpack_location(v8::Handle<v8::Object>& location_obj)
 
 location unpack_location(Nan::NAN_METHOD_ARGS_TYPE info)
 {
-    v8::Handle<v8::Object> location_obj = v8::Handle<v8::Object>::Cast(info[0]);
+    v8::Handle<v8::Object> location_obj = info[0].As<v8::Object>();
     return unpack_location(location_obj);
 }
 
@@ -70,12 +71,12 @@ NAN_METHOD(CalculateResults) {
     std::vector<location> locations; //we'll get this from Node.js
     std::vector<rain_result> results; //we'll build this in C++
 
-    v8::Local<v8::Array> input = v8::Local<v8::Array>::Cast(info[0]);
+    v8::Local<v8::Array> input = info[0].As<v8::Array>();
 
     unsigned int num_locations = input->Length();
     for(unsigned int i = 0; i < num_locations; i++)
     {
-        v8::Local<v8::Object> m = v8::Local<v8::Object>::Cast(Nan::Get(input, Nan::New(i)).ToLocalChecked());
+        v8::Local<v8::Object> m = Nan::Get(input, Nan::New(i)).ToLocalChecked().As<v8::Object>();
         locations.push_back(
                 unpack_location(m)
         );
@@ -102,11 +103,91 @@ NAN_METHOD(CalculateResults) {
     info.GetReturnValue().Set(result_list);
 }
 
+struct Work {
+    uv_work_t request;
+    Nan::Persistent<v8::Function> callback;
+
+    std::vector<location> *locations;
+    std::vector<rain_result> *results;
+};
+
+static void WorkAsync(uv_work_t *req)
+{
+    Work *work = static_cast<Work *>(req->data);
+
+    //This is the worker thread, lets bulid up the results
+    //allocated results from the heap because we'll need
+    //to accesss in the event loop laber to send back
+    work->results = new std::vector<rain_result>();
+    work->results->resize(work->locations->size());
+    std::transform(work->locations->begin(),
+            work->locations->end(),
+            work->results->begin(),
+            calc_rain_stats);
+
+    //that wasn't really that long of an opeation,
+    //so lets pretend it took longer ...
+
+    sleep(3);
+}
+
+//called by libuv in event loop when async function completes
+static void WorkAsyncComplete(uv_work_t *req, int status)
+{
+    Work *work = static_cast<Work *>(req->data);
+
+
+    //the work has been done, and now we pack the results 
+    //vector into a local array on the event-thread's stack
+    v8::Local<v8::Array> result_list = Nan::New<v8::Array>();
+    for(unsigned int i = 0; i < work->results->size(); i++)
+    {
+        v8::Local<v8::Object> result = Nan::New<v8::Object>();
+        pack_rain_result(result, (*(work->results))[i]);
+        result_list->Set(i, result);
+    }
+
+    //set up return arguments
+    v8::Local<v8::Value> argv[] = { result_list };
+    v8::Local<v8::Function> clbk = Nan::New(work->callback);
+
+    Nan::MakeCallback(Nan::GetCurrentContext()->Global(), clbk, 1, argv);
+
+    delete work;
+}
+
+NAN_METHOD(CalculateResultsAsync) {
+    Work *work = new Work();
+    work->request.data = work;
+
+    //Locations is on the heap, accessible in the libuv threads
+    work->locations = new std::vector<location>();
+    v8::Local<v8::Array> input = info[0].As<v8::Array>();
+    unsigned int num_locations = input->Length();
+    for(unsigned int i = 0; i < num_locations; i++)
+    {
+        v8::Handle<v8::Object> m = input->Get(i).As<v8::Object>();
+        work->locations->push_back(
+            unpack_location(m)
+        );
+    }
+
+    //store the callback from JS in the work package so we can invoke it later
+    v8::Local<v8::Function> prst = info[1].As<v8::Function>();
+    work->callback.Reset(prst);
+
+    //kick of the worker thread
+    uv_queue_work(uv_default_loop(), &work->request, WorkAsync, WorkAsyncComplete);
+
+    info.GetReturnValue().Set(Nan::Undefined());
+}
+
 
 NAN_MODULE_INIT(init) {
     Nan::Set(target, Nan::New("avg_rainfall").ToLocalChecked(), Nan::New<v8::FunctionTemplate>(AvgRainfall)->GetFunction());
     Nan::Set(target, Nan::New("data_rainfall").ToLocalChecked(), Nan::New<v8::FunctionTemplate>(RainfallData)->GetFunction());
     Nan::Set(target, Nan::New("calculate_results").ToLocalChecked(), Nan::New<v8::FunctionTemplate>(CalculateResults)->GetFunction());
+    Nan::Set(target, Nan::New("calculate_results_async").ToLocalChecked(), Nan::New<v8::FunctionTemplate>(CalculateResultsAsync)->GetFunction());
 }
 
 
--- rainfall.js
+++ rainfall.js
@@ -4,6 +4,23 @@ var makeup = function(max) {
     return Math.round(max * Math.random() * 100) / 100;
 }
 
+var print_rain_results = function (results) {
+    //report the results from C++
+    var i = 0;
+    results.forEach(function(result) {
+            console.log("Result for location " + i);
+            console.log("------------------------------");
+            console.log("\tLatitude:        " + locations[i].latitude.toFixed(2));
+            console.log("\tLongitude:       " + locations[i].longitude.toFixed(2));
+            console.log("\tMean Rainfall:   " + result.mean.toFixed(2) + " cm");
+            console.log("\tMedian Rainfall: " + result.median.toFixed(2) + " cm");
+            console.log("\tStandard dev.:   " + result.standard_deviation.toFixed(2));
+            console.log("\tNumber Samples:  " + result.n);
+            console.log();
+            i++;
+    });
+}
+
 
 //Build some dummy locations
 var locations = [];
@@ -23,20 +40,7 @@ for(var i = 0; i < 10; i++) {
 
 //Invoke the Addon
 var results = rainfall.calculate_results(locations);
-console.log(locations);
-console.log(results);
+print_rain_results(results);
 
-//report the results from C++
-var i = 0;
-results.forEach(function(result) {
-        console.log("Result for location " + i);
-        console.log("------------------------------");
-        console.log("\tLatitude:        " + locations[i].latitude.toFixed(2));
-        console.log("\tLongitude:       " + locations[i].longitude.toFixed(2));
-        console.log("\tMean Rainfall:   " + result.mean.toFixed(2) + " cm");
-        console.log("\tMedian Rainfall: " + result.median.toFixed(2) + " cm");
-        console.log("\tStandard dev.:   " + result.standard_deviation.toFixed(2));
-        console.log("\tNumber Samples:  " + result.n);
-        console.log();
-        i++;
-});
+//pass a callback into the addon, and have the addon return immediately
+rainfall.calculate_results_async(locations, print_rain_results);
